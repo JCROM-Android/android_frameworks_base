32f048505d33c04ecbc0be77f82939d6deadd673
Akihiro Maeda <sola.1980.a@gmail.com>
2014-12-02 02:01:10 +0900

    Modified to be able to change the config_sms_capable

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -85,6 +85,7 @@ public class TelephonyManager {
     private final Context mContext;
 
     private static final String VOICE_CAPABLE_PROPERTY = "persist.sys.voice.capable";
+    private static final String SMS_CAPABLE_PROPERTY = "persist.sys.sms.capable";
 
     private static String multiSimConfig =
             SystemProperties.get(TelephonyProperties.PROPERTY_MULTI_SIM_CONFIG);
@@ -2389,8 +2390,9 @@ public class TelephonyManager {
      */
     public boolean isSmsCapable() {
         if (mContext == null) return true;
-        return mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_sms_capable);
+        //return mContext.getResources().getBoolean(
+        //        com.android.internal.R.bool.config_sms_capable);
+        return SystemProperties.getBoolean(SMS_CAPABLE_PROPERTY, true);
     }
 
     /**
